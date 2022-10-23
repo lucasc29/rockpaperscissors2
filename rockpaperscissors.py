8c3402e135a2240e4b050672e113353ad4542980
lucasc29 <115600876+lucasc29@users.noreply.github.com>
2022-10-23 16:36:51 +1100

    Create rockpaperscissors.py

---
+++ rockpaperscissors.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.title('Rock Paper Scissor Game...')
+
+player = st.text_input("Select Rock, Paper, or Scissor :").lower()
+
+if st.button("Begin Game"):
+    computer = random.choice(["Rock 🪨", "Paper 📄", "Scissor ✄"]).lower()
+    st.write("Computer selected: ", computer)
+    if player == "rock" and computer == "paper":
+        st.write("Computer Won... 📄 beats 🪨")
+    elif player == "paper" and computer == "scissor":
+        st.write("Computer Won... ✄ beats 📄")
+    elif player == "scissor" and computer == "rock":
+        st.write("Computer Won... 🪨 beats✄")
+    elif player == computer:
+        st.write("You both chose the same item")
+    else:
+        st.write("You beat the computer")
+else:
+    st.write("After typing your choice, hit enter and then the \'begin game\' button")
