aee21e87d4971932b5f8b1cdc2c39bcb96572ba6
lucasc29 <115600876+lucasc29@users.noreply.github.com>
2022-10-23 17:12:50 +1100

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -5,7 +5,24 @@ st.title('The Rock Paper Scissors Game')
 
 player = st.text_input("Select Rock, Paper, or Scissor :").lower()
 
+
 if st.button("Begin Game"):
+    def check_user_input(player):
+        try:
+            # Convert it into integer
+            val = str(input)
+            st.write("Input Accepted...", val)
+        except ValueError:
+            try:
+                # Convert it into float
+                val = float(input)
+                st.write("Input is a float  number. Number = ", val)
+            except ValueError:
+                st.write("No.. input is not number. It's a string")
+
+check_user_input(player)
+
+
     if player == "rock" or player == "paper" or  player == "scissors":
         computer = random.choice(["Rock", "Paper", "Scissor"]).lower()
         st.write("Computer selected: ", computer)
